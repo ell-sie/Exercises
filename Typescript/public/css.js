2719bf7408019b701848f1448276a8f5c8c45a82
ll-sie <e.ndiramiye@alustudent.com>
2023-09-11 15:30:14 +0200

    kata

--- Typescript/public/css.js
+++ Typescript/public/css.js
@@ -2,14 +2,17 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.compare = void 0;
 function compare(a, b) {
-    // let str = a + b
-    // let count = 0
-    // for(let i = 0; i < str.length; i++){
-    //   if(str[i] === '#' || str[i]  === '.' ){
-    //    count ++ 
-    //   }    
-    // }
-    if (a.length > b.length) {
+    let str = a + b;
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === '#' || str[i] === '.') {
+            count++;
+        }
+    }
+    if (a >= b) {
+        return a;
+    }
+    else if (a.length > b.length) {
         return a;
     }
     else if (a.includes('#') && b.includes('.')) {
---
+++ Typescript/public/roman.js
@@ -0,0 +1,37 @@
+"use strict";
+function rangeToRoman(endRoman) {
+    // Define Roman numerals and their corresponding values
+    const numerals = {
+        M: 1000,
+        CM: 900,
+        D: 500,
+        CD: 400,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1,
+    };
+    const result = [];
+    let currentNumber = 0;
+    while (endRoman !== '') {
+        for (const numeral of Object.keys(numerals).reverse()) {
+            if (endRoman.startsWith(numeral)) {
+                currentNumber += numerals[numeral];
+                result.push(currentNumber);
+                endRoman = endRoman.slice(numeral.length);
+                currentNumber = 0; // Reset currentNumber after adding it to the result
+                break;
+            }
+        }
+    }
+    return result;
+}
+console.log(rangeToRoman('X')); // [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log(rangeToRoman('IV')); // [0, 1, 2, 3]
+console.log(rangeToRoman('III')); // [0, 1, 2]
+console.log(rangeToRoman('XIX')); // [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
